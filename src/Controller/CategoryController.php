3bff707e2f1c555d6c68672083505f6c1a005a3b
Vivien Piveteau <vivien.piveteau@hotmail.fr>
2018-11-12 17:30:48 +0100

    Symfony7

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -8,6 +8,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,4 +25,22 @@ class CategoryController extends AbstractController
     {
         return $this->render('category.html.twig', ['category' => $category,]);
     }
+    
+    /**
+     * @Route("/categories/articles", name="categoriesArticles_show")
+     * @return Response
+     */
+    public function index (): Response
+    {
+        $categories=$this->getDoctrine()
+        ->getRepository(Category::class)
+        ->findAll();
+        
+        if(!$categories) {
+            throw $this->createNotFoundException('No categorie found in categorie\'s table');
+        }
+        
+        return $this->render('categoryArticles.html.twig', ['categories' => $categories ]);
+    }
+    
 }
